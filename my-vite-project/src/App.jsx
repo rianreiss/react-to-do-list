eaa799e4e1bcd29aaa304cda6774617a0cbd2717
rianreiss <rianreis2109@gmail.com>
2024-01-14 16:07:25 -0300

    first commit

---
+++ my-vite-project/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir pra academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ])
+
+  return <div className='app'>
+    <h1>Task List</h1>
+    <div className="todo-list">
+      {todos.map((todo) => (
+        <div className="todo">
+          <div className="content">
+            <p>{todo.text}</p>
+            <p className="category">
+              ({todo.category})
+            </p>
+          </div>
+          <div>
+            <button>Complete</button>
+            <button>x</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>;
+}
+
+export default App
