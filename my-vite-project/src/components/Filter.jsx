624bba8865fc3078fbe385b1b0a6f5e9637a70a2
rianreiss <rianreis2109@gmail.com>
2024-01-20 01:37:47 -0300

    saving some updates

--- my-vite-project/src/components/Filter.jsx
+++ my-vite-project/src/components/Filter.jsx
@@ -1,22 +1,44 @@
-const Filter = ({ filter, setFilter, setSort }) => {
-    return (
-        <div className="filter">
-            <h2>Filter:</h2>
-            <div className="filter-options">
-                <div>
-                    <p>Status:</p>
-                    <select value={filter} onChange={(e) => setFilter(e.target.value)}>
-                        <option value="all">All</option>
-                        <option value="completed">Completed</option>
-                        <option value="inCompleted">Not Completed</option>
-                    </select>
-                </div>
+import { useState } from 'react'
+import filterSvg from '../assets/funnel.svg';
 
-                <div>
-                    <p>Alphabetical  Ordem:</p>
-                    <button onClick={() => setSort("Asc")}>Asc</button>
-                    <button onClick={() => setSort("Desc")}>Desc</button>
-                </div>
+const Filter = ({ filter, setFilter, setSort }) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const handleToggle = () => {
+        setIsExpanded(!isExpanded);
+    }
+
+
+    return (
+        <div className="filter-container">
+            <div onClick={handleToggle} className='filter-title'>
+                <img src={filterSvg} alt='teste' />
+                <h2>Filtrar</h2>
+            </div>
+            <div>
+                {isExpanded ? (
+                    <div>
+                        <div className={`filter-options ${isExpanded ? 'visible' : ''}`}>
+                            <div>
+                                <p>Status:</p>
+                                <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+                                    <option value="all">All</option>
+                                    <option value="completed">Completed</option>
+                                    <option value="inCompleted">Not Completed</option>
+                                </select>
+                            </div>
+
+                            <div>
+                                <p>Alphabetical  Ordem:</p>
+                                <button onClick={() => setSort("Asc")}>Asc</button>
+                                <button onClick={() => setSort("Desc")}>Desc</button>
+                            </div>
+                        </div>
+                    </div>
+
+                ) : (
+                    ""
+                )}
             </div>
         </div>
     );
