f8428259202c471fa46085e67ad493c22730c3d0
rianreiss <rianreis2109@gmail.com>
2024-01-22 23:00:40 -0300

    completed tasks count added

--- my-vite-project/src/App.jsx
+++ my-vite-project/src/App.jsx
@@ -89,28 +89,32 @@ function App() {
           <EmptyList />
         </div>
       ) : (
-        todos
-          .filter((todo) =>
-            filter === "all"
-              ? true
-              : filter === "completed"
-                ? todo.isCompleted
-                : !todo.isCompleted
-          )
-          .filter((todo) =>
-            todo.text.toLowerCase().includes(search.toLowerCase()))
-          .sort((a, b) =>
-            sort === "Asc"
-              ? a.text.localeCompare(b.text)
-              : b.text.localeCompare(a.text)
-          )
-          .map((todo) => (
-            <Todo
-              key={todo.id}
-              todo={todo}
-              removeTodo={removeTodo}
-              completeTodo={completeTodo} />
-          )))}
+        <div>
+          <p className='completedTasks'>Tarefes concluídas: {todos.filter((todo) => todo.isCompleted).length}/{todos.length}.</p>
+          {todos
+            .filter((todo) =>
+              filter === "all"
+                ? true
+                : filter === "completed"
+                  ? todo.isCompleted
+                  : !todo.isCompleted
+            )
+            .filter((todo) =>
+              todo.text.toLowerCase().includes(search.toLowerCase()))
+            .sort((a, b) =>
+              sort === "Asc"
+                ? a.text.localeCompare(b.text)
+                : b.text.localeCompare(a.text)
+            )
+            .map((todo) => (
+              <Todo
+                key={todo.id}
+                todo={todo}
+                removeTodo={removeTodo}
+                completeTodo={completeTodo} />
+            ))}
+        </div>
+      )}
     </div>
 
     <TodoForm addTodo={addTodo} />
