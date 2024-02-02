3e587360400fdd09ffe628d14ebd8cc88e5269c3
rianreiss <rianreis2109@gmail.com>
2024-02-01 23:55:24 -0300

    Filter UI Updates

--- my-vite-project/src/components/Filter.jsx
+++ my-vite-project/src/components/Filter.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import filterSvg from '../assets/funnel.svg';
 import resertSvg from '../assets/reset-arrow.svg';
 
 const Filter = ({ filter, setFilter, setSort, categoryFilter, setCategoryFilter, priorityFilter, setPriorityFilter }) => {
-    const [isExpanded, setIsExpanded] = useState(false);
+    const [filterContainer, setFilterContainer] = useState(false);
 
-    const handleToggle = () => {
-        setIsExpanded(!isExpanded);
-    }
+
+    const openFilter = () => {
+        setFilterContainer(!filterContainer);
+
+    };
 
     const resetFilters = () => {
         setFilter('all');
@@ -19,62 +22,64 @@ const Filter = ({ filter, setFilter, setSort, categoryFilter, setCategoryFilter,
     return (
         <div className="filter-container">
             <div className='filter-title'>
-                <div onClick={handleToggle}>
+                <div onClick={openFilter}>
                     <img src={filterSvg} />
                     <h2>Filtrar</h2>
                 </div>
-                { filter == "all" && categoryFilter == "" && priorityFilter == "" ? (
+                {filter == "all" && categoryFilter == "" && priorityFilter == "" ? (
                     ""
                 ) : (
-                    
+
                     <img src={resertSvg} onClick={resetFilters} title='Resetar filtros' />
                 )}
             </div>
-            <div className={`teste ${isExpanded ? 'visible' : ''}`}>
-                {isExpanded ? (
-                    <div>
-                        <div className={`filter-options`}>
-                            <div>
-                                <p>Status:</p>
-                                <select value={filter} onChange={(e) => setFilter(e.target.value)}>
-                                    <option value="all">Todas</option>
-                                    <option value="completed">Completas</option>
-                                    <option value="inCompleted">Não Completas</option>
-                                </select>
-                            </div>
 
-                            <div>
-                                <p>Ordem Alfabética:</p>
-                                <button onClick={() => setSort("Asc")}>Crescente</button>
-                                <button onClick={() => setSort("Desc")}>Decrescente</button>
-                            </div>
+            <CSSTransition
+                in={filterContainer}
+                timeout={300}
+                classNames="openFilter"
+                unmountOnExit
 
-                            <div>
-                                <p>Categoria:</p>
-                                <select value={categoryFilter} onChange={(e) => setCategoryFilter(e.target.value)}>
-                                    <option value="">Todas</option>
-                                    <option value="Trabalho">Trabalho</option>
-                                    <option value="Pessoal">Pessoal</option>
-                                    <option value="Estudos">Estudos</option>
-                                </select>
-                            </div>
+            >
+                <div>
+                    <div className={`filter-options`}>
+                        <div>
+                            <p>Status:</p>
+                            <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+                                <option value="all">Todas</option>
+                                <option value="completed">Completas</option>
+                                <option value="inCompleted">Não Completas</option>
+                            </select>
+                        </div>
 
-                            <div>
-                                <p>Prioridade:</p>
-                                <select value={priorityFilter} onChange={(e) => setPriorityFilter(e.target.value)}>
-                                    <option value="">Todas</option>
-                                    <option value="1">Alta</option>
-                                    <option value="2">Normal</option>
-                                    <option value="3">Baixa</option>
-                                </select>
-                            </div>
+                        <div>
+                            <p>Ordem Alfabética:</p>
+                            <button onClick={() => setSort("Asc")}>Crescente</button>
+                            <button onClick={() => setSort("Desc")}>Decrescente</button>
+                        </div>
+
+                        <div>
+                            <p>Categoria:</p>
+                            <select value={categoryFilter} onChange={(e) => setCategoryFilter(e.target.value)}>
+                                <option value="">Todas</option>
+                                <option value="Trabalho">Trabalho</option>
+                                <option value="Pessoal">Pessoal</option>
+                                <option value="Estudos">Estudos</option>
+                            </select>
+                        </div>
+
+                        <div>
+                            <p>Prioridade:</p>
+                            <select value={priorityFilter} onChange={(e) => setPriorityFilter(e.target.value)}>
+                                <option value="">Todas</option>
+                                <option value="1">Alta</option>
+                                <option value="2">Normal</option>
+                                <option value="3">Baixa</option>
+                            </select>
                         </div>
                     </div>
-
-                ) : (
-                    ""
-                )}
-            </div>
+                </div>
+            </CSSTransition>
         </div>
     );
 };
